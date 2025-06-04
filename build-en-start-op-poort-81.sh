081facfbb99386db6a5ac929c11abdbe9942dc07
MediFlow <147162590+majdi3112@users.noreply.github.com>
2025-06-05 00:07:38 +0200

    Update build-en-start-op-poort-81.sh

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-# Container naam en image tag
+
 CONTAINER_NAME="demo-container"
 IMAGE_TAG="demo-site:latest"
 
-# Bouw de Docker image met tag demo-site:latest
 docker build -t ${IMAGE_TAG} .
 
-# Stop een eventueel bestaande container met dezelfde naam
 docker stop ${CONTAINER_NAME} || true
 docker rm ${CONTAINER_NAME} || true
 
-# Start een nieuwe container
+
 docker run -d -t -p 81:80 --name ${CONTAINER_NAME} ${IMAGE_TAG} 
